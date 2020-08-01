ef2d6649301b845688c1844bcc27a9816c9765a4
lucaslomba <lucasazevedolomba@gmail.com>
2020-08-01 10:30:18 -0300

    projeto finalizado

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: "AqJKAJ0TKms",
+        title: "Criando um PWA com React Native & Expo Web | Code/Drops #34",
+        duration: "42 min",
+        price: "free",
+        featured: true
+    },
+    {
+        id: "SGwHpzgqzgk",
+        title: "Criando um Carousel do Zero só com CSS | Code/Drops #39",
+        duration: "25 min",
+        price: "free"
+    },
+    {
+        id: "Pbs1VIwPoRA",
+        title: "Consumindo APIs no React com SWR | Code/Drops #38",
+        duration: "37 min",
+        price: "free"
+    },
+    {
+        id: "DDm0M_rZLJo",
+        title: "Clonando interface do Nubank (+ ANIMAÇÕES) com React Native | Diego Fernandes",
+        duration: "1 hora e 13 min",
+        price: "free"
+    },
+    {
+        id: "GOB-lawExXc",
+        title: "React Hooks com TypeScript | Masterclass #04",
+        duration: "48 min",
+        price: "free",
+        featured: true
+    },
+    {
+        id: "IPLTUlRFxgo",
+        title: "Configurando CI em projeto open-source com Github Actions | Behind the Code #12",
+        duration: "46 min",
+        price: "free",
+        featured: true
+    }
+]
